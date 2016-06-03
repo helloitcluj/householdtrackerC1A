decff87ab8a155e6d6437166882f2addf606f852
Student <biropacsazsolt@yahoo.ie>
2016-06-03 20:40:02 +0300

    Fixing bad merge

--- ux/src/main/java/com/helloit/householdtracker/ux/spring/LoginController.java
+++ ux/src/main/java/com/helloit/householdtracker/ux/spring/LoginController.java
@@ -1,9 +1,6 @@
 package com.helloit.householdtracker.ux.spring;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
@@ -12,11 +9,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-<<<<<<< HEAD
-@Service
-@Controller
-@RequestMapping("op")
-=======
+
 /**
  * Created by Administrator on 5/25/2016.
  */
@@ -24,45 +17,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Service
 @Controller
 @RequestMapping("login")
->>>>>>> origin/master
+
 public class LoginController {
 
     private static final Logger LOGGER = LogManager.getLogger(LoginController.class);
 
     private static final String MESSAGE_PARAMETER_TAG = "message";
-<<<<<<< HEAD
-    private static final String HELLO_VIEW_TAG = "op";
-    private static final String SAMPLE_TEXT = "Szia HelloIT ";
-
-    //@Resource
-    ///public IAccountRepository accountRepository;
-
-    //@Transactional
-    @RequestMapping(method = RequestMethod.GET)
-=======
     private static final String Login_Page = "login";
     private static final String SAMPLE_TEXT = "Szia HelloIT ";
 
     @RequestMapping(method = RequestMethod.POST)
->>>>>>> origin/master
+
     public String printWelcome(String name, final ModelMap model) {
 
         LOGGER.info(name);
 
-<<<<<<< HEAD
 
-       /* final Account entity = new Account();
-        entity.setName("hello");
-        entity.setPassword("world");
-        final Account savedEntity = accountRepository.save(entity);*/
-
-        //model.addAttribute(MESSAGE_PARAMETER_TAG, SAMPLE_TEXT);
-
-        return HELLO_VIEW_TAG;
-    }
-}
-=======
         return Login_Page;
     }
 }
->>>>>>> origin/master
+
