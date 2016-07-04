b740de9ce42e82a051524deb2398b2ce39dfc433
asinoai <aron.sinoai@linkonomics.com>
2016-07-04 13:55:14 +0300

    UX updates

--- ux/src/main/webapp/js/common.js
+++ ux/src/main/webapp/js/common.js
@@ -2,24 +2,24 @@ var helloit = helloit || {};
 helloit.createNavbar = function () {
     var $result = $('\
     <nav class="navbar navbar-light bg-faded">\
-    <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse"\
-    data-target="#exCollapsingNavbar2">\
-    &#9776;\
-    </button>\
-    <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">\
-    <a class="navbar-brand" href="#">Household tracker</a>\
-    <ul class="nav navbar-nav">\
-    <li class="nav-item">\
-    <a class="nav-link" href="index.html">Home<span class="sr-only">(current)</span></a>\
-    </li>\
-    <li class="nav-item">\
-    <a class="nav-link" href="expense.html">Expense</a>\
-    </li>\
-    <li class="nav-item pull-xs-right">\
-    <a class="nav-link" id="logout">Logout</a>\
-    </li>\
-    </ul>\
-    </div>\
+        <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse"\
+        data-target="#exCollapsingNavbar2">\
+        &#9776;\
+        </button>\
+        <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">\
+            <a class="navbar-brand" href="#">Household tracker</a>\
+            <ul class="nav navbar-nav">\
+                <li class="nav-item">\
+                    <a class="nav-link" href="index.html">Home</a>\
+                </li>\
+                <li class="nav-item">\
+                    <a class="nav-link" href="expense.html">Expense</a>\
+                </li>\
+                <li class="nav-item pull-xs-right">\
+                    <a class="nav-link" id="logout">Logout</a>\
+                </li>\
+                </ul>\
+        </div>\
     </nav>\
     ');
 
