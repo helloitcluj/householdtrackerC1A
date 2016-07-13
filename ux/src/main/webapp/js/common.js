4b2efb57297dd5f9fa9d9eb746b16a6824260ea3
timea7 <timea7@yahoo.com>
2016-07-13 20:47:33 +0300

    Prep expenses ui for expenses table

--- ux/src/main/webapp/js/common.js
+++ ux/src/main/webapp/js/common.js
@@ -1,7 +1,7 @@
 var helloit = helloit || {};
 helloit.createNavbar = function () {
     var $result = $('\
-    <nav class="navbar navbar-light bg-faded">\
+    <nav class="navbar navbar-light bg-faded ">\
         <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse"\
         data-target="#exCollapsingNavbar2">\
         &#9776;\
